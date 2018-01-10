aa95a62925a2654f1ec1fb8ee80969a16624c75d
pit <pit@snowgem.org>
2018-01-10 20:54:20 +0700

    Remove unneccesary files

--- node_modules/bitcoinjs-lib-zcash/src/templates/witnesspubkeyhash/input.js
+++
@@ -1,9 +0,0 @@
-// {signature} {pubKey}
-
-var pkh = require('../pubkeyhash/input')
-
-module.exports = {
-  check: pkh.check,
-  decodeStack: pkh.decodeStack,
-  encodeStack: pkh.encodeStack
-}
--- node_modules/bitcoinjs-lib-zcash/src/templates/witnessscripthash/input.js
+++
@@ -1,9 +0,0 @@
-// {signature} {pubKey}
-
-var p2sh = require('../scripthash/input')
-
-module.exports = {
-  check: p2sh.check,
-  decodeStack: p2sh.decodeStack,
-  encodeStack: p2sh.encodeStack
-}
